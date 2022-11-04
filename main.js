e4e4eefa93ef71536349078fc825b99676a56fa4
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-04 18:58:21 -0500

    added person Obj and todos array

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+// Objects
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  hobbies: ["music", "movies", "sports"],
+  address: {
+    street: "50 main st",
+    city: "Boston",
+    state: "MA",
+  },
+};
 
+const todos = [
+  {
+    id: 1,
+    text: "Take out trash",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    text: "Meeting with boss",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    text: "Dentist appt",
+    isCompleted: true,
+  },
+];
+
+console.log(todos[1].text);
