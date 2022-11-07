e576ddc15c9b5ffade7d35f829734ea24a7023d8
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-06 18:57:54 -0600

    added JS code

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+/ ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES / /
+  // LOGGING OUTPUT
+  alert("Hello World"); // Do not use for debugging. Stops script and only strings
+console.log("Hello World");
+console.error("This is an error");
+console.warn("This is a warning");
+
+// VARIABLES - var, let, const
+let age = 30;
+
+// let can be re-assigned, const can not
+age = 31;
+
+// DATA TYPES - String, Number, Boolean, null, undefined
+const name = "Brad";
+const age3 = 37;
+const rating = 3.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; // undefined
+
+// Check type
+console.log(typeof z);
+
 // Objects
 const person = {
   firstName: "John",
   lastName: "Doe",
-  age: 30,
+  age2: 30,
   hobbies: ["music", "movies", "sports"],
   address: {
     street: "50 main st",
