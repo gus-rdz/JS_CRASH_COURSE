24817fe32282195a1473a09e8a0259d26ad8f223
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-07 20:15:48 -0600

    added concatenation code

--- main.js
+++ main.js
@@ -23,6 +23,26 @@ let z; // undefined
 // Check type
 console.log(typeof z);
 
+// STRINGS
+
+// Concatenation
+console.log("My name is " + name + " and I am " + age);
+// Template literal (better)
+console.log(`My name is ${name} and I am ${age}`);
+
+// String methods & properties
+const s = "Hello World";
+let val;
+// Get length
+val = s.length;
+// Change case
+val = s.toUpperCase();
+val = s.toLowerCase();
+// Get sub string
+val = s.substring(0, 5);
+// Split into array
+val = s.split("");
+
 // Objects
 const person = {
   firstName: "John",
