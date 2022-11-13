55f4837cc5afe057ec2217e46de23d3d614856a0
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-12 19:56:59 -0600

    added functions

--- main.js
+++ main.js
@@ -191,3 +191,18 @@ switch (color) {
 
 // Ternary operator / Shorthand if
 const z = color === "red" ? 10 : 20;
+
+// FUNCTIONS
+function greet(greeting = "Hello", name) {
+  if (!name) {
+    // console.log(greeting);
+    return greeting;
+  } else {
+    // console.log(`${greeting} ${name}`);
+    return `${greeting} ${name}`;
+  }
+}
+
+// ARROW FUNCTIONS
+const greet = (greeting = "Hello", name = "There") => `${greeting} ${name}`;
+console.log(greet("Hi"));
