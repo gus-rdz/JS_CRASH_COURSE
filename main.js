d850c34af904dcf6989047d88b2fc6da1935dede
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-13 14:18:38 -0600

    added OOP

--- main.js
+++ main.js
@@ -206,3 +206,38 @@ function greet(greeting = "Hello", name) {
 // ARROW FUNCTIONS
 const greet = (greeting = "Hello", name = "There") => `${greeting} ${name}`;
 console.log(greet("Hi"));
+
+// OOP
+
+// Constructor Function
+function Person(firstName, lastName, dob) {
+  // Set object properties
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob); // Set to actual date object using Date constructor
+  // this.getBirthYear = function(){
+  //   return this.dob.getFullYear();
+  // }
+  // this.getFullName = function() {
+  //   return `${this.firstName} ${this.lastName}`
+  // }
+}
+
+// Get Birth Year
+Person.prototype.getBirthYear = function () {
+  return this.dob.getFullYear();
+};
+
+// Get Full Name
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+// Instantiate an object from the class
+const person1 = new Person("John", "Doe", "7-8-80");
+const person2 = new Person("Steve", "Smith", "8-2-90");
+
+console.log(person2);
+
+// console.log(person1.getBirthYear());
+// console.log(person1.getFullName());
