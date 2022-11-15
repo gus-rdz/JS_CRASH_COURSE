e5a6abdfd42cc1564f0b532f79dbf0b204dc1d4e
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-14 19:24:56 -0600

    added Element Selectors

--- main.js
+++ main.js
@@ -269,3 +269,16 @@ class Person {
 
 const person1 = new Person("John", "Doe", "7-8-80");
 console.log(person1.getBirthYear());
+
+// ELEMENT SELECTORS
+
+// Single Element Selectors
+console.log(document.getElementById("my-form"));
+console.log(document.querySelector(".container"));
+// Multiple Element Selectors
+console.log(document.querySelectorAll(".item"));
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("item"));
+
+const items = document.querySelectorAll(".item");
+items.forEach((item) => console.log(item));
