71c93a69dd998ed80c6137c7098f444724bfbb06
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-15 20:02:56 -0600

    DOM manipulation

--- main.js
+++ main.js
@@ -282,3 +282,14 @@ console.log(document.getElementsByClassName("item"));
 
 const items = document.querySelectorAll(".item");
 items.forEach((item) => console.log(item));
+
+// MANIPULATING THE DOM
+const ul = document.querySelector(".items");
+// ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = "Hello";
+ul.children[1].innerText = "Brad";
+ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
+
+const btn = document.querySelector(".btn");
+// btn.style.background = 'red';
