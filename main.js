7c86a23509777eacf57d6848e8f8248af62e51f6
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-16 21:08:26 -0600

    added Events

--- main.js
+++ main.js
@@ -293,3 +293,20 @@ ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
 
 const btn = document.querySelector(".btn");
 // btn.style.background = 'red';
+
+// EVENTS
+
+// Mouse Event
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(e.target.className);
+  document.getElementById("my-form").style.background = "#ccc";
+  document.querySelector("body").classList.add("bg-dark");
+  ul.lastElementChild.innerHTML = "<h1>Changed</h1>";
+});
+
+// Keyboard Event
+const nameInput = document.querySelector("#name");
+nameInput.addEventListener("input", (e) => {
+  document.querySelector(".container").append(nameInput.value);
+});
