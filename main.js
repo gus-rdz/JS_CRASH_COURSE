d3bc627a5d743a056d7c68a56dd5920b806e4550
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-22 21:31:50 -0600

    added Honda function

--- main.js
+++ main.js
@@ -203,6 +203,24 @@
 //   }
 // }
 
+// function myFunc(theObject) {
+//   theObject.make = 'Toyota';
+// }
+
+// const mycar = {
+//   make: 'Honda',
+//   model: 'Accord',
+//   year: 1998,
+// };
+
+// // x gets the value "Honda"
+// const x = mycar.make;
+
+// // the make property is changed by the function
+// myFunc(mycar);
+// // y gets the value "Toyota"
+// const y = mycar.make;
+
 // // ARROW FUNCTIONS
 // const greet = (greeting = "Hello", name = "There") => `${greeting} ${name}`;
 // console.log(greet("Hi"));
