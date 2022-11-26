fd71bc689d1f93580a5ce5d1ad1060127c965d92
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-26 17:34:51 -0600

    added leapyear and date finder

--- main.js
+++ main.js
@@ -443,3 +443,20 @@
 // console.log(today);
 // today = dd+'/'+mm+'/'+yyyy;
 // console.log(today);
+
+// console.log("--------------------");
+// for (var year = 2014; year <= 2050; year++) {
+//   var d = new Date(year, 0, 1);
+//   if (d.getDay() === 0) console.log("1st January is being a Sunday  " + year);
+// }
+// console.log("--------------------");
+
+// function leapyear(year)
+// {
+// return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+// }
+// console.log(leapyear(2016));
+// console.log(leapyear(2000));
+// console.log(leapyear(1700));
+// console.log(leapyear(1800));
+// console.log(leapyear(100));
