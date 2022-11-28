5b5068432685340feb643dcaeb8fd91d53b50a9b
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-27 21:29:46 -0600

    more recursion practice

--- main.js
+++ main.js
@@ -494,3 +494,27 @@
 // };
 
 // console.log(range(2,9));
+
+// var array_sum = function (my_array) {
+//   if (my_array.length === 1) {
+//     return my_array[0];
+//   } else {
+//     return my_array.pop() + array_sum(my_array);
+//   }
+// };
+
+// console.log(array_sum([1, 2, 3, 4, 5, 6]));
+
+// var exponent = function(a, n)
+// {
+//    if (n === 0)
+//    {
+//     return 1;
+//     }
+//   else
+//   {
+//     return a * exponent(a, n-1);
+//   }
+// };
+
+// console.log(exponent(4, 2));
