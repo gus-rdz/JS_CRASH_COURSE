56dd684967747880fc8e6dd2fbf5ccf30dea3831
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-11-28 22:25:16 -0600

    added multiplier program

--- main.js
+++ main.js
@@ -518,3 +518,16 @@
 // };
 
 // console.log(exponent(4, 2));
+
+// Multiply Program
+// function multiplyBy() {
+//   num1 = document.getElementById("firstNumber").value;
+//   num2 = document.getElementById("secondNumber").value;
+//   document.getElementById("result").innerHTML = num1 * num2;
+// }
+
+// function divideBy() {
+//   num1 = document.getElementById("firstNumber").value;
+//   num2 = document.getElementById("secondNumber").value;
+//   document.getElementById("result").innerHTML = num1 / num2;
+// }
