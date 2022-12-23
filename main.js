fd737d5ef7ca022cb35990f01728e0067029a977
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-12-22 19:31:57 -0600

    added ES6 specified value program for type check

--- main.js
+++ main.js
@@ -875,3 +875,19 @@
 // console.log(isArrayLike(document.querySelectorAll('.className'))); // true
 // console.log(isArrayLike('abc')); // true
 // console.log(isArrayLike(null)); // false
+
+// Check whether the provided value is of the specified type
+// const is = (type, val) => ![, null].includes(val) && val.constructor === type;
+// console.log(is(Array, [1])); // true
+// console.log(is(ArrayBuffer, new ArrayBuffer()));
+// console.log(is(Map, new Map()));
+// console.log(is(RegExp, /./g));
+// console.log(is(Set, new Set()));
+// console.log(is(WeakMap, new WeakMap()));
+// console.log(is(WeakSet, new WeakSet()));
+// console.log(is(String, ""));
+// console.log(is(String, new String("")));
+// console.log(is(Number, 1));
+// console.log(is(Number, new Number(1)));
+// console.log(is(Boolean, true));
+// console.log(is(Boolean, new Boolean(true)));
