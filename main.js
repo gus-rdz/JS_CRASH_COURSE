6067a1c25bc1584298cc6bafbab5fc7df01ee558
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-12-30 17:14:36 -0600

    added array problem to find NaN indexes

--- main.js
+++ main.js
@@ -1009,3 +1009,26 @@
 // var items = [1, 2, 3, 4, 5, 7, 8, 9];
 // console.log(binary_Search(items, 1));
 // console.log(binary_Search(items, 5));
+
+// Get all the indexes where NaN is found of a given array of numbers and NaN
+// function test(arr_nums){
+//   return arr_nums.map(function(el, i){
+//       if(isNaN(el))
+//         return i;
+//       return false;
+//   }).filter(function(el){
+//       return el;
+//   });
+// }
+// arr_nums = [2, NaN, 8, 16, 32]
+// console.log("Original array: "+arr_nums)
+// result = test(arr_nums)
+// console.log("Find all indexes of NaN of the said array: "+result);
+// arr_nums = [2, 4, NaN, 16, 32, NaN]
+// console.log("Original array: "+arr_nums)
+// result = test(arr_nums)
+// console.log("Find all indexes of NaN of the said array: "+result);
+// arr_nums = [2, 4, 16, 32]
+// console.log("Original array: "+arr_nums)
+// result = test(arr_nums)
+// console.log("Find all indexes of NaN of the said array: "+result);
