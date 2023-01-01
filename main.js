0d2852984969620edd001304dce4ccdddaa4d1e6
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-12-31 18:27:47 -0600

    added Quick Sort program

--- main.js
+++ main.js
@@ -1032,3 +1032,33 @@
 // console.log("Original array: "+arr_nums)
 // result = test(arr_nums)
 // console.log("Find all indexes of NaN of the said array: "+result);
+
+// Sort a list of elements using Quick sort.
+// function quick_Sort(origArray) {
+// 	if (origArray.length <= 1) {
+// 		return origArray;
+// 	} else {
+
+// 		var left = [];
+// 		var right = [];
+// 		var newArray = [];
+// 		var pivot = origArray.pop();
+// 		var length = origArray.length;
+
+// 		for (var i = 0; i < length; i++) {
+// 			if (origArray[i] <= pivot) {
+// 				left.push(origArray[i]);
+// 			} else {
+// 				right.push(origArray[i]);
+// 			}
+// 		}
+
+// 		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+// 	}
+// }
+
+// var myArray = [3, 0, 2, 5, -1, 4, 1 ];
+
+// console.log("Original array: " + myArray);
+// var sortedArray = quick_Sort(myArray);
+// console.log("Sorted array: " + sortedArray);
