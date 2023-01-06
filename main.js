a4137849bdb9dca1d98518823619604496e69566
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-05 22:31:29 -0600

    added a program that lists properties of JS Obj

--- main.js
+++ main.js
@@ -1062,3 +1062,17 @@
 // console.log("Original array: " + myArray);
 // var sortedArray = quick_Sort(myArray);
 // console.log("Sorted array: " + sortedArray);
+
+// list the properties of a JavaScript object
+// function _keys(obj) {
+//   if (!isObject(obj)) return [];
+//   if (Object.keys) return Object.keys(obj);
+//   var keys = [];
+//   for (var key in obj) if (_.has(obj, key)) keys.push(key);
+//   return keys;
+// }
+// function isObject(obj) {
+//   var type = typeof obj;
+//   return type === "function" || (type === "object" && !!obj);
+// }
+// console.log(_keys({ red: "#FF0000", green: "#00FF00", white: "#FFFFFF" }));
