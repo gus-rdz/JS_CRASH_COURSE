a2036e5f4a4179cd8fec2724ced0f5604c3b50de
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-06 19:18:30 -0600

    added new array program

--- main.js
+++ main.js
@@ -1076,3 +1076,25 @@
 //   return type === "function" || (type === "object" && !!obj);
 // }
 // console.log(_keys({ red: "#FF0000", green: "#00FF00", white: "#FFFFFF" }));
+
+// Moves one element of an array to another
+// function move(arr, old_index, new_index) {
+//   while (old_index < 0) {
+//       old_index += arr.length;
+//   }
+//   while (new_index < 0) {
+//       new_index += arr.length;
+//   }
+//   if (new_index >= arr.length) {
+//       var k = new_index - arr.length;
+//       while ((k--) + 1) {
+//           arr.push(undefined);
+//       }
+//   }
+//    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+//  return arr;
+// }
+
+// console.log(move([10, 20, 30, 40, 50], 0, 2));
+
+// console.log(move([10, 20, 30, 40, 50], -1, -2));
