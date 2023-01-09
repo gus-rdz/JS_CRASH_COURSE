4fb1549d782e31aad0f96bfc657e499110e6e470
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-08 20:13:40 -0600

    added bit manipulation that turns off kth bit in given number

--- main.js
+++ main.js
@@ -1114,3 +1114,23 @@
 // console.log(check_even_odd(4));
 // console.log(check_even_odd(9));
 // console.log(check_even_odd("15"));
+
+// Turn off the kth bit in a given number. Return the new number
+// const turn_Off_Kth_Bit = (n, k) => {
+//   if (typeof n != "number") {
+//     return "It must be number!";
+//   }
+//   return n & ~(1 << (k - 1));
+// };
+// n = 30;
+// k = 3;
+// console.log(n + " in binary is " + n.toString(2));
+// console.log("Turning k'th bit off," + " k = " + k);
+// result_n = turn_Off_Kth_Bit(n, k);
+// console.log(result_n + " in binary is " + result_n.toString(2));
+// n = 100;
+// k = 6;
+// console.log(n + " in binary is " + n.toString(2));
+// console.log("Turning k'th bit off," + " k = " + k);
+// result_n = turn_Off_Kth_Bit(n, k);
+// console.log(result_n + " in binary is " + result_n.toString(2));
