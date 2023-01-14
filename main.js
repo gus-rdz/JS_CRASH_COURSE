049cffd96afab47e8c970d837230d431f04bf8d7
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-13 20:36:47 -0600

    added a ternary search algo

--- main.js
+++ main.js
@@ -1196,3 +1196,34 @@
 // console.log(is_IP("198.156.23.5"));
 
 // console.log(is_IP("172.16.0.1"));
+
+// Find the first index of a given element in an array using the ternary search algorithm
+// const ternary_Search = (arr, target, left, right) => {
+// 	if (right >= left) {
+// 		let mid1 = Math.floor(left + (right - left) /3);
+// 		let mid2 = Math.floor(right - (right - left) /3);
+
+// 		if (arr[mid1] == target) {
+// 			return mid1;
+// 		}
+// 		else if (arr[mid2] == target) {
+// 			return mid2;
+// 		}
+
+// 		if (target < arr[mid1]) {
+// 			return ternary_Search(arr, target, left, mid1 - 1);
+// 		}
+// 		else if (target > arr[mid2]) {
+// 			return ternary_Search(arr, target, mid2 + 1, right);
+// 		}
+// 		else {
+// 			return ternary_Search(arr, target, mid1 + 1, mid2 - 1);
+// 		}
+// 	}
+// 	return -1;
+// }
+
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 0, 5))
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 9, 0, 8))
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 0, 8))
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4, 6, 8))
