5de12b60435a43a65f7966248ee0f6d489e542a7
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-19 18:07:53 -0600

    added string function that truncates specified numbers

--- main.js
+++ main.js
@@ -1338,3 +1338,21 @@
 // console.log(is_caPostalCode("K8V 3Y1"));
 
 // console.log(is_caPostalCode("Z4V4X1"));
+
+// Function to truncate a string if it is longer than the specified number of characters
+// text_truncate = function (str, length, ending) {
+//   if (length == null) {
+//     length = 100;
+//   }
+//   if (ending == null) {
+//     ending = "...";
+//   }
+//   if (str.length > length) {
+//     return str.substring(0, length - ending.length) + ending;
+//   } else {
+//     return str;
+//   }
+// };
+// console.log(text_truncate("We are doing JS string exercises."));
+// console.log(text_truncate("We are doing JS string exercises.", 19));
+// console.log(text_truncate("We are doing JS string exercises.", 15, "!!"));
