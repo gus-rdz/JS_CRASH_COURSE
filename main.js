dd93179e8579ca3589c97c6162081ce2afb7ebc8
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-22 16:47:54 -0600

    added Bit manipulation exercise

--- main.js
+++ main.js
@@ -1415,3 +1415,20 @@
 //     return type === 'function' || type === 'object' && !!obj;
 //   }
 // console.log(key_value_pairs({red: "#FF0000", green: "#00FF00", white: "#FFFFFF"}));
+
+// Function Bit Manipulation Exercise
+// const log_Two = (n) => {
+//   if (typeof n!= "number") {
+//       return 'It must be number!'
+//       }
+//    let result = 0
+//    while (n >> 1) {
+//      n >>= 1
+//      result++
+//    }
+//    return result
+//  }
+//  console.log(log_Two(1))
+//  console.log(log_Two(2))
+//  console.log(log_Two(4))
+//  console.log(log_Two(32))
