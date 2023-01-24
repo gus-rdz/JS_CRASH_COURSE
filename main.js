e78d67f81a612f1c962e654851e4f635855c4b1a
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-24 17:50:15 -0600

    added Validation func that checks given values are same type or not

--- main.js
+++ main.js
@@ -1485,3 +1485,20 @@
 // numList.empty_list()
 // console.log("Check the said list is empty or not!")
 // console.log(numList.is_Empty())
+
+// Function to check whether given value types are same or not
+// function is_sameType(value1, value2) {
+//   if(is_nan(value1) || is_nan(value2)) {
+//    return is_nan(value1) === is_nan(value2);
+//   }
+//   return toString.call(value1) === toString.call(value2);
+// }
+
+// function is_nan(val)
+//   {
+//   return val !== val;
+//  }
+
+// console.log(is_sameType('12', 100));
+// console.log(is_sameType('12', '100'));
+// console.log(is_sameType(12, 100));
