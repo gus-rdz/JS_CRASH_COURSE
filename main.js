d555309774a82ed4066c94c8aab3957934a02397
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-26 16:40:08 -0600

    added search algorithm exercisze

--- main.js
+++ main.js
@@ -1520,3 +1520,34 @@
 // console.log(is_dateString("01/22/2015"));
 
 // console.log(is_dateString("32/01/2015"));
+
+// first index of a given element in an array using the ternary search algorithm
+// const ternary_Search = (arr, target, left, right) => {
+// 	if (right >= left) {
+// 		let mid1 = Math.floor(left + (right - left) /3);
+// 		let mid2 = Math.floor(right - (right - left) /3);
+
+// 		if (arr[mid1] == target) {
+// 			return mid1;
+// 		}
+// 		else if (arr[mid2] == target) {
+// 			return mid2;
+// 		}
+
+// 		if (target < arr[mid1]) {
+// 			return ternary_Search(arr, target, left, mid1 - 1);
+// 		}
+// 		else if (target > arr[mid2]) {
+// 			return ternary_Search(arr, target, mid2 + 1, right);
+// 		}
+// 		else {
+// 			return ternary_Search(arr, target, mid1 + 1, mid2 - 1);
+// 		}
+// 	}
+// 	return -1;
+// }
+
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 0, 5))
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 9, 0, 8))
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 0, 8))
+// console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4, 6, 8))
