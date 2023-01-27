b60690fffd3c9bb09bd7e0c1a6b5d528f8e915ef
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-27 16:29:42 -0600

    added a sorting algorithm for Gnome sort

--- main.js
+++ main.js
@@ -1551,3 +1551,28 @@
 // console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 9, 0, 8))
 // console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 0, 8))
 // console.log(ternary_Search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4, 6, 8))
+
+// Sort a list of elements using Gnome sort
+// function gnomeSort(arr)
+// {
+//     function moveBack(i)
+//   {
+//         for( ; i > 0 && arr[i-1] > arr[i]; i--)
+//         {
+//             var t = arr[i];
+//             arr[i] = arr[i-1];
+//             arr[i-1] = t;
+//         }
+//     }
+//     for (var i = 1; i < arr.length; i++)
+//     {
+//         if (arr[i-1] > arr[i]) moveBack(i);
+//     }
+//     return arr;
+// }
+
+// var arra = [3, 0, 2, 5, -1, 4, 1];
+// console.log("Original Array Elements");
+// console.log(arra);
+// console.log("Sorted Array Elements");
+// console.log(gnomeSort(arra));
