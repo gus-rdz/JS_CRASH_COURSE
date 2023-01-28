7a0d56f325d87e61a25f1fa7b80c37003e066805
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-28 03:52:41 -0600

    added program on bit manipulation finds power of two

--- main.js
+++ main.js
@@ -1576,3 +1576,25 @@
 // console.log(arra);
 // console.log("Sorted Array Elements");
 // console.log(gnomeSort(arra));
+
+// Program to find next power of two of a given number
+// const next_Power_Of_Two = (num) => {
+//   if (typeof num != "number") {
+//       return 'It must be number!'
+//   }
+
+// if (num > 0 && (num & (num - 1)) === 0)
+// return num
+// let result = 1
+
+// while (num > 0)
+// {
+// result = result << 1
+// num = num >> 1
+// }
+// return result
+// }
+// console.log(next_Power_Of_Two(1))
+// console.log(next_Power_Of_Two(4))
+// console.log(next_Power_Of_Two(9))
+// console.log(next_Power_Of_Two("15"))
