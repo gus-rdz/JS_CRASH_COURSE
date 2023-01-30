6c8449dd84b970507aaa7ce534183bb9186f2588
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-01-29 18:37:20 -0600

    added a bit manipulation program that swaps 2 bits

--- main.js
+++ main.js
@@ -1598,3 +1598,19 @@
 // console.log(next_Power_Of_Two(4))
 // console.log(next_Power_Of_Two(9))
 // console.log(next_Power_Of_Two("15"))
+
+// Swaps 2 bits
+// const swap_bits = (n, pos1, pos2) => {
+//   if (typeof n!= "number") {
+//          return 'It must be number!'
+//      }
+//   if ((((n & (1 << pos1)) >> pos1) ^ ((n & (1 << pos2)) >> pos2)) == 1)
+//        {
+//            n ^= (1 << pos1);
+//            n ^= (1 << pos2);
+//        }
+//   return n;
+// }
+// console.log(swap_bits(245,1,4))
+// console.log(swap_bits(137,6,7))
+// console.log(swap_bits("16"))
