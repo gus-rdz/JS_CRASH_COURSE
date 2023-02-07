c447c61cd959a96515d3d2616cfd682c2391ac84
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-02-06 19:40:26 -0600

    added palindrome program using recursion

--- main.js
+++ main.js
@@ -1921,3 +1921,27 @@
 // console.log("Original Array: " + nums);
 // result = odd_Even_Sort(nums);
 // console.log("Sorted Array: " + result);
+
+// Recursion for Palindrome program
+// const test = (text) => {
+//   if (typeof text !== 'string') {
+//         return 'String should not be empty!'
+//   }
+
+//   if (text.length <= 1) {
+//     return true
+//   }
+
+//   if (text[0] !== text[text.length - 1])
+//   {
+//     return false
+//   }
+//   else
+//   {
+//     return test(text.slice(1, text.length - 1))
+//   }
+// }
+// console.log(test("madam"))
+// console.log(test("abdb"))
+// console.log(test("ab"))
+// console.log(test("a"))
