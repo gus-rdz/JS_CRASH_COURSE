c888997365a1002eca8ac31cefc86c4cd476bc03
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-02-07 21:39:43 -0600

    added JS Math that calculates the sum of all odd elements

--- main.js
+++ main.js
@@ -1945,3 +1945,19 @@
 // console.log(test("abdb"))
 // console.log(test("ab"))
 // console.log(test("a"))
+
+// JS Math - calculate the sum of all odd elements in a square matrix
+// function test(nums) {
+//     return nums.reduce((b,a) => [...b,...a], []).reduce((b,a) => !(a%2) ? b: a+b , 0)
+// }
+// nums = [
+//         [1, 2, 3],
+//         [4, 5, 6],
+//         [7, 8, 9]
+//        ]
+// console.log("Sum of all odds of the said matrix: "+test(nums));
+// nums = [
+//         [-1, -2],
+//         [-4, -5]
+//        ]
+// console.log("Sum of all odds of the said matrix: "+test(nums));
