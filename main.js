fc5f0dca37826581c2a224b408bea602bff6d5ab
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2023-02-09 17:49:07 -0600

    added a JS Date function to get ISO-8601 numeric representation

--- main.js
+++ main.js
@@ -1973,3 +1973,15 @@
 // console.log(is_Natural(-15));
 // console.log(is_Natural(1));console.log(is_Natural(10.22));
 // console.log(is_Natural(10/0));
+
+// JS Date for ISO-8601 numeric representation
+// function ISO_numeric_date(dt)
+// {
+//   return (dt.getDay() === 0 ? 7 : dt.getDay());
+// }
+
+// dt = new Date();
+// console.log(ISO_numeric_date(dt));
+
+// dt = new Date(2015, 10, 1);
+// console.log(ISO_numeric_date(dt));
